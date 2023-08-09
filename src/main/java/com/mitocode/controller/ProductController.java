599a9d27d96dc85cd3c7a5902870ac6e0b5830d9
Boris Vargas <borisvargaspaucara@gmail.com>
2023-08-08 23:14:20 -0400

    subiendo cambios

--- src/main/java/com/mitocode/controller/ProductController.java
+++ src/main/java/com/mitocode/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.mitocode.controller;
 import com.mitocode.model.Product;
 import com.mitocode.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,26 @@ public class ProductController {
     @GetMapping
     public List<Product> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable("id") int id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public int save(@RequestBody Product product) {
+        return service.save(product);
+    }
+
+    @PutMapping
+    public int update(@RequestBody Product product) {
+        return service.update(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public int deleteById(@PathVariable("id") int id) {
+        return service.deleteById(id);
     }
 
 }
